2f2aff184c42af72a97f38ae5e6470fac27a13ab
HarveySpecter <devanshshah1309@gmail.com>
2022-06-05 18:28:58 +0800

    Add GroupAnagrams solution

---
+++ Medium/GroupAnagrams.py
@@ -0,0 +1,18 @@
+from re import I
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            c = str(sorted(s))
+            if c in d:
+                d[c].append(s)
+            else:
+                d[c] = [s]
+        return list(d.values())
+                
+
+s = Solution()
+print(s.groupAnagrams(["eat", "tea", "bag"]))
