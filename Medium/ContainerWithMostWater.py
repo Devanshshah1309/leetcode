3819769ff3688e8c48b4a97d1bd1394611664fa7
HarveySpecter <devanshshah1309@gmail.com>
2022-06-15 20:20:10 +0800

    Add comments for Container With Most Water

--- Medium/ContainerWithMostWater.py
+++ Medium/ContainerWithMostWater.py
@@ -1,3 +1,4 @@
+# O(n) time and O(1) space
 from typing import List
 
 
@@ -6,6 +7,7 @@ class Solution:
         i: int = 0
         j: int = len(height) - 1
         maxArea: int = 0
+        # we decrement j when height[i] is greater than height[j] because incrementing i would not affect min(height[i], height[j]) but it would rather also decrease the width of the rectangle. In short, we remove the bottleneck that is weighing down the min(height[i], height[j]) by decreasing the width by 1 for a potential gain in min(height[i], height[j]) which would overcompensate and hence lead to larger area.
         while i < j:
             curr: int = (j - i)*min(height[i], height[j])
             if curr > maxArea:
